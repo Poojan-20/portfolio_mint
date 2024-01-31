2f8a8f656d8ac86ecced0eef9ab1dc1d13bb7c17
Poojan-20 <poojanp2008@gmail.com>
2024-01-31 10:22:40 +0530

    change: about me added

--- components/about.tsx
+++ components/about.tsx
@@ -1,28 +1,33 @@
-'use client';
-import React, { useEffect } from 'react'
-import SectionHeading from './sectionheading'
+"use client";
+import React, { useEffect } from "react";
+import SectionHeading from "./sectionheading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
-
 export default function About() {
-   const {ref} = useSectionInView('About');
-    
-    return (
-        <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.175 }}
-            id='about'
-            ref={ref}
-        >
-            <SectionHeading>About me</SectionHeading>
-            <p className='mb-3 text-center'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere itaque est quia reiciendis distinctio explicabo iure a asperiores, enim hic praesentium aspernatur obcaecati. Non iure possimus, quod veniam corporis porro.
-            </p>
-            <p className='mb-3 text-center'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt aut sapiente cumque. Enim maxime illum odit! Neque odit natus provident iure, et quia tempora est omnis consectetur dicta perferendis veritatis atque possimus?
-            </p>
-        </motion.section>
-    )
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+      ref={ref}
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3 text-center">
+        An avid developer navigating the tech waves at Jio Private Limited. By
+        day, I'm crafting and delivering tech wonders, infusing creativity into
+        projects that push boundaries.
+      </p>
+      <p className="mb-3 text-center">
+        But when it's time to unwind, gaming is my brain fuzz remedy. Armed with
+        Valorant, a keyboard, and a cup of coffee are my power ups, always one
+        respawn away. I am always on the lookout for new music artists.I am not
+        just building projects but I am basically a tech storyteller.Let's connect !!
+      </p>
+    </motion.section>
+  );
 }
